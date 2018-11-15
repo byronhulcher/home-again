42c42287f22b54d78a2aaca840003f66f315425d
Byron Hulcher <byronhulcher@gmail.com>
2018-11-14 22:13:32 -0500

    Clean-up pt 1

--- public/client.js
+++ public/client.js
@@ -1,372 +1,371 @@
-(function() {
-  let graphpaper = new Graphpaper(),
-    random = Math.random.bind(),
-    setInterval = window.setInterval.bind(),
-    compassEl = document.querySelector('.compass'),
-    starEl = document.querySelector('.star'),
-    playerEl = document.querySelector('.player'),
-    stepMilliseconds = 666,
-    cellSize = 25,
-    cameraTrackRange = 7,
-    camera = {
-      x: 0,
-      y: 0
-    },
-    player = {},
-    paused = false,
-    interval,
-    lastSavedState;
-    
-  
-  function saveToLocalStorage() {
-    let saveState = window.btoa(JSON.stringify(player));
+/* global Graphpaper */
+(function () {
+  const graphpaper = new Graphpaper()
+  const random = Math.random.bind()
+  const setInterval = window.setInterval.bind()
+  const compassEl = document.querySelector('.compass')
+  const starEl = document.querySelector('.star')
+  const playerEl = document.querySelector('.player')
+  const stepMilliseconds = 666
+  const cellSize = 25
+  const cameraTrackRange = 7
 
-    lastSavedState = saveState;
+  let camera = {
+    x: 0,
+    y: 0
+  }
+  let player = {}
+  let paused = false
+  let interval
+  let lastSavedState
+
+  function saveToLocalStorage () {
+    let saveState = window.btoa(JSON.stringify(player))
+
+    lastSavedState = saveState
 
     try {
-      localStorage.setItem('player', saveState);
+      window.localStorage.setItem('player', saveState)
     } catch (e) {}
   }
-  
-  function resetCamera() {
-    camera.x = player.x;
-    camera.y = player.y;
-  }
-  
-  function loadFromLocalStorage() {
+
+  function loadFromLocalStorage () {
     try {
-      player = JSON.parse(window.atob(localStorage.getItem('player')));
-    } catch (e) {}
-    
-    if (!player || typeof player.x == "undefined" || typeof player.y == "undefined") {
-      resetPlayer();
-    }
-    
-    resetCamera();
-  }
-  
-  function hasSaveDataBeenModified() {
-    let saveState;
-    
-    try {
-      saveState = localStorage.getItem('player');
+      player = JSON.parse(window.atob(window.localStorage.getItem('player')))
     } catch (e) {}
 
-    if (typeof lastSavedState !== "undefined" && typeof saveState !== "undefined"){
-       return  lastSavedState !== saveState;
+    if (!player || typeof player.x === 'undefined' || typeof player.y === 'undefined') {
+      resetPlayer()
     }
 
-    return false; 
+    resetCamera()
   }
-  
-  function resetPlayer() {
+
+  function hasSaveDataBeenModified () {
+    let saveState
+
+    try {
+      saveState = window.localStorage.getItem('player')
+    } catch (e) {}
+
+    if (typeof lastSavedState !== 'undefined' && typeof saveState !== 'undefined') {
+      return lastSavedState !== saveState
+    }
+
+    return false
+  }
+
+  function resetCamera () {
+    camera.x = player.x
+    camera.y = player.y
+  }
+
+  function resetPlayer () {
     player = {
       steps: 0,
       x: 0,
       y: 0,
       won: false,
-      submitted: false,
+      submitted: false
     }
-    saveToLocalStorage();
+
+    saveToLocalStorage()
   }
-  
-  function edgeOfView(rect, theta) {
-    var twoPI = Math.PI*2;
+
+  function edgeOfView (rect, theta) {
+    const twoPI = Math.PI * 2
 
     while (theta < -Math.PI) {
-      theta += twoPI;
+      theta += twoPI
     }
 
     while (theta > Math.PI) {
-      theta -= twoPI;
+      theta -= twoPI
     }
 
-    var rectAtan = Math.atan2(rect.height, rect.width);
-    var tanTheta = Math.tan(theta);
-    var region;
+    const rectAtan = Math.atan2(rect.height, rect.width)
+    const tanTheta = Math.tan(theta)
+    let region
 
     if ((theta > -rectAtan) && (theta <= rectAtan)) {
-        region = 1;
+      region = 1
     } else if ((theta > rectAtan) && (theta <= (Math.PI - rectAtan))) {
-        region = 2;
+      region = 2
     } else if ((theta > (Math.PI - rectAtan)) || (theta <= -(Math.PI - rectAtan))) {
-        region = 3;
+      region = 3
     } else {
-        region = 4;
+      region = 4
     }
 
-    var edgePoint = {x: rect.width/2, y: rect.height/2};
-    var xFactor = 1;
-    var yFactor = 1;
+    let edgePoint = { x: rect.width / 2, y: rect.height / 2 }
+    let xFactor = 1
+    let yFactor = 1
 
     switch (region) {
-      case 1: yFactor = -1; break;
-      case 2: yFactor = -1; break;
-      case 3: xFactor = -1; break;
-      case 4: xFactor = -1; break;
+      case 1: yFactor = -1; break
+      case 2: yFactor = -1; break
+      case 3: xFactor = -1; break
+      case 4: xFactor = -1; break
     }
 
     if ((region === 1) || (region === 3)) {
-      edgePoint.x += xFactor * (rect.width / 2.);                                     // "Z0"
-      edgePoint.y += yFactor * (rect.width / 2.) * tanTheta;
+      edgePoint.x += xFactor * (rect.width / 2.0) // "Z0"
+      edgePoint.y += yFactor * (rect.width / 2.0) * tanTheta
     } else {
-      edgePoint.x += xFactor * (rect.height / (2. * tanTheta));                        // "Z1"
-      edgePoint.y += yFactor * (rect.height /  2.);
+      edgePoint.x += xFactor * (rect.height / (2.0 * tanTheta)) // "Z1"
+      edgePoint.y += yFactor * (rect.height / 2.0)
     }
 
-    return edgePoint;
+    return edgePoint
   };
-  
-  function updateCompass() {
-    var angleRadians = Math.atan2( player.y, -1 * player.x);
-    compassEl.setAttribute('style', `transform: rotate(${angleRadians}rad); left: ${501.0 / 2.0 - (cellSize * (camera.x - player.x))}px; bottom: ${501.0 / 2.0 - (cellSize * (camera.y - player.y))}px;`);
-    compassEl.classList.toggle('hidden', (player.won || (player.x == 0 && player.y == 0 )));
-    
-  }
-  
-  function updateOrigin() {
-    if (Math.abs(camera.x) > 10 || Math.abs(camera.y) > 10) {
-      var angleRadians = Math.atan2( (camera.y), -1 * (camera.x)),
-        locationOnSquare = edgeOfView({height: 521.0, width: 521.0}, angleRadians);
-      starEl.setAttribute('style', `left: ${locationOnSquare.x - 10}px; bottom: ${locationOnSquare.y - 16}px;`);
-      starEl.classList.add('offscreen');
-    } else {
-      starEl.setAttribute('style', `bottom: ${501.0 / 2.0 - (cellSize * (camera.y))}px; left: ${501.0 / 2.0 - (cellSize * (camera.x))}px`);
-      starEl.classList.remove('offscreen');
-      
-    }
-    starEl.classList.toggle('pulsing', (player.won));
-    starEl.classList.toggle('hidden', (player.x == 0 && player.y == 0 && !player.won));
-    
+
+  function updateCompass () {
+    const angleRadians = Math.atan2(player.y, -1 * player.x)
+
+    compassEl.setAttribute('style', `transform: rotate(${angleRadians}rad); left: ${501.0 / 2.0 - (cellSize * (camera.x - player.x))}px; bottom: ${501.0 / 2.0 - (cellSize * (camera.y - player.y))}px;`)
+    compassEl.classList.toggle('hidden', (player.won || (player.x === 0 && player.y === 0)))
   }
 
-  function updateUX() {
-    graphpaper.setProps({offsetX: camera.x * cellSize * -1, offsetY: camera.y * cellSize});
-    playerEl.setAttribute('style', `left: ${501.0 / 2.0 - (cellSize * (camera.x - player.x))}px; bottom: ${501.0 / 2.0 - (cellSize * (camera.y - player.y)) + 4}px;`);
+  function updateOrigin () {
+    if (Math.abs(camera.x) > 10 || Math.abs(camera.y) > 10) {
+      const angleRadians = Math.atan2((camera.y), -1 * (camera.x))
+      const locationOnSquare = edgeOfView({ height: 521.0, width: 521.0 }, angleRadians)
+
+      starEl.setAttribute('style', `left: ${locationOnSquare.x - 10}px; bottom: ${locationOnSquare.y - 16}px;`)
+      starEl.classList.add('offscreen')
+    } else {
+      starEl.setAttribute('style', `bottom: ${501.0 / 2.0 - (cellSize * (camera.y))}px; left: ${501.0 / 2.0 - (cellSize * (camera.x))}px`)
+      starEl.classList.remove('offscreen')
+    }
+
+    starEl.classList.toggle('pulsing', (player.won))
+    starEl.classList.toggle('hidden', (player.x === 0 && player.y === 0 && !player.won))
+  }
+
+  function updateUX () {
+    graphpaper.setProps({ offsetX: camera.x * cellSize * -1, offsetY: camera.y * cellSize })
+    playerEl.setAttribute('style', `left: ${501.0 / 2.0 - (cellSize * (camera.x - player.x))}px; bottom: ${501.0 / 2.0 - (cellSize * (camera.y - player.y)) + 4}px;`)
 
     document.querySelector('header .location').innerHTML = `( ${player.x}, ${player.y} )`
     document.querySelector('header .steps').innerHTML = `${player.steps} Steps`
-    
-    document.querySelector('.graph-container .ux').classList.toggle('hidden', !player.won);
-    
+
+    document.querySelector('.graph-container .ux').classList.toggle('hidden', !player.won)
+
     if (player.won || player.gameover) {
-      document.querySelector('.graph-container .score').innerHTML = `After ${player.steps} steps`;
+      document.querySelector('.graph-container .score').innerHTML = `After ${player.steps} steps`
     }
-    
+
     document.querySelector('.ux .you-win').classList.toggle('hidden', !player.won || !!player.gameover)
     document.querySelector('.ux .game-over').classList.toggle('hidden', !player.won || !player.gameover)
-    
+
     document.querySelector('.ux .submit').classList.toggle('hidden', !!(player.submitted || player.gameover))
     document.querySelector('.ux .reset').classList.toggle('hidden', !player.submitted && !player.gameover)
-    
-    updateCompass();
-    updateOrigin();
+
+    updateCompass()
+    updateOrigin()
   }
-  
-  function createGraph() {
+
+  function createGraph () {
     graphpaper.setProps({
-      "width": (cellSize * 20) + 1,
-      "height": (cellSize * 20) + 1,
-      "cellWidth": cellSize,
-      "cellHeight": cellSize,
-      "minorColor": [
+      'width': (cellSize * 20) + 1,
+      'height': (cellSize * 20) + 1,
+      'cellWidth': cellSize,
+      'cellHeight': cellSize,
+      'minorColor': [
         0.63671875,
         0.765625,
         0.95703125,
         1
       ],
-      "majorColor": [
+      'majorColor': [
         0.84765625,
         0.1953125,
         0.1953125,
         1
       ],
-      "majorRows": 10,
-      "majorCols": 10,
-      "alpha": 1,
-      "offsetX": player.x * cellSize,
-      "offsetY": player.y * cellSize,
-      "canvasElement": null
-    }); 
-    
-    
-    document.querySelector('.graph-container').appendChild(graphpaper.element);
+      'majorRows': 10,
+      'majorCols': 10,
+      'alpha': 1,
+      'offsetX': player.x * cellSize,
+      'offsetY': player.y * cellSize,
+      'canvasElement': null
+    })
+
+    document.querySelector('.graph-container').appendChild(graphpaper.element)
   }
-  
-  function resetGame() {
-    if (!player.won && !window.confirm("Are you sure?")){
-      return;
+
+  function resetGame () {
+    if (!player.won && !window.confirm('Are you sure?')) {
+      return
     }
-    clearTicks();
-    resetPlayer(); 
-    resetCamera();
-    updateUX();
-    startTicks();
+
+    clearTicks()
+    resetPlayer()
+    resetCamera()
+    updateUX()
+    startTicks()
   }
-  
-  function setupButtons() {
-     document.addEventListener('click', function (event) {
+
+  function setupButtons () {
+    document.addEventListener('click', function (event) {
       if (event.target.classList.contains('reset')) {
-        resetGame();
+        resetGame()
       }
-       
+
       if (event.target.classList.contains('submit')) {
-        submitHighScore();
+        submitHighScore()
       }
     })
   }
-  
-  function getRandomInt(max) {
-    return Math.floor(random() * Math.floor(max));
-  }
-  
-  function updateCamera() {
+
+  function updateCamera () {
     if (player.x >= camera.x + cameraTrackRange) {
-      camera.x = camera.x + 1;
+      camera.x = camera.x + 1
     } else if (player.x <= camera.x - cameraTrackRange) {
-      camera.x = camera.x - 1;
+      camera.x = camera.x - 1
     }
-    
+
     if (player.y >= camera.y + cameraTrackRange) {
-      camera.y = camera.y + 1;
+      camera.y = camera.y + 1
     } else if (player.y <= camera.y - cameraTrackRange) {
-      camera.y = camera.y - 1;
+      camera.y = camera.y - 1
     }
   }
-  
-  function updatePlayer() {
-    let move = getRandomInt(4);
+
+  function updatePlayer () {
+    const move = Math.floor(random() * Math.floor(4)) // random int
 
     switch (move) {
       case 0:
-        player.x = player.x + 1;
-        break;
+        player.x = player.x + 1
+        break
       case 1:
-        player.x = player.x - 1;
-        break;
+        player.x = player.x - 1
+        break
       case 2:
-        player.y = player.y + 1;
-        break;
+        player.y = player.y + 1
+        break
       case 3:
-        player.y = player.y - 1;
-        break;
+        player.y = player.y - 1
+        break
       default:
-        console.log("how did you get here?");
+        console.log('how did you get here?')
     }
-    
-    player.steps += 1;
-    
+
+    player.steps += 1
+
     if (player.x === 0 && player.y === 0) {
-     player.won = true; 
+      player.won = true
     }
   };
-  
-  function gameOver() {
-    player.won = true;
-    player.gameover = true;  
+
+  function gameOver () {
+    player.won = true
+    player.gameover = true
   }
 
-  function onTick() {
+  function onTick () {
     if (hasSaveDataBeenModified()) {
-      gameOver();
+      gameOver()
     } else {
-      updatePlayer();
-      saveToLocalStorage();
-    }
-    
-    if (player.won) {
-      resetCamera();
-      clearTicks();
+      updatePlayer()
+      saveToLocalStorage()
     }
 
-    updateCamera();
-    updateUX();
+    if (player.won) {
+      resetCamera()
+      clearTicks()
+    }
+
+    updateCamera()
+    updateUX()
   }
-  
-  function clearTicks() {
-    clearInterval(interval);
+
+  function clearTicks () {
+    clearInterval(interval)
   }
-  
-  function htmlEncode(s){
-    return s.replace(/&(?!\w+([;\s]|$))/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+  function htmlEncode (s) {
+    return s.replace(/&(?!\w+([;\s]|$))/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
   }
-  
-  function updateHighScoreTable(scores) {
+
+  function updateHighScoreTable (scores) {
     document.body.querySelector('.high-score-container tbody').innerHTML = `
       ${scores.map((item, i) => `
         <tr><td>${htmlEncode(item.name)}</td><td>${item.score.toLocaleString()}</td></tr>
       `).join('')}
-    `;
-    return scores;
+    `
+    return scores
   }
-  
-  function getHighScores() {
-    return fetch('/scores/', {
+
+  function getHighScores () {
+    return window.fetch('/scores/', {
       method: 'GET',
       credentials: 'same-origin'
     })
-    .then(response => response.json())
-    .catch(error => [])
-    .then(scores => updateHighScoreTable(scores));
+      .then(response => response.json())
+      .catch(() => [])
+      .then(scores => updateHighScoreTable(scores))
   }
-  
-  function submitHighScore() {
-    let name = "",
-      highScores = [];
-    
+
+  function submitHighScore () {
+    const maxNameLength = 20
+    let name = ''
+    let highScores = []
+
     while (name.length <= 0) {
-      name = window.prompt("What's your name? [Max length: 20]"); 
-    }
-    
-    name = name.slice(0, 20);
-    
-    getHighScores()
-    .then(scores => {
-      highScores = scores;
-      return fetch('/scores/', {
-        body: JSON.stringify({steps: player.steps, name: name}),
-        credentials: 'same-origin',
-        headers: {
-          'content-type': 'application/json'
-        },
-        method: 'POST'
-      })
-    })
-    .then(response => response.json())
-    .catch(error => highScores)
-    .then(scores => {
-      player.submitted = true;
-      updateHighScoreTable(scores);
-      updateUX();
-      saveToLocalStorage();
-    });
-  }
-  
-  function startTicks() {
-    if (!paused){
-      interval = setInterval(onTick, stepMilliseconds);
-    }
-  }
-  
-  function main() {
-    loadFromLocalStorage();
-  
-    if (!(player && player.steps)){
-      resetPlayer(); 
+      name = window.prompt(`What's your name? [Max length: ${maxNameLength}]`)
     }
 
-    createGraph();
-    
-    setupButtons();
-    
-    updateUX();
-    
-    getHighScores();
-    
-    if (!player.won){
-      startTicks(); 
+    name = name.slice(0, maxNameLength)
+
+    getHighScores()
+      .then(scores => {
+        highScores = scores
+        return window.fetch('/scores/', {
+          body: JSON.stringify({ steps: player.steps, name: name }),
+          credentials: 'same-origin',
+          headers: {
+            'content-type': 'application/json'
+          },
+          method: 'POST'
+        })
+      })
+      .then(response => response.json())
+      .catch(() => highScores)
+      .then(scores => {
+        player.submitted = true
+        updateHighScoreTable(scores)
+        updateUX()
+        saveToLocalStorage()
+      })
+  }
+
+  function startTicks () {
+    if (!paused) {
+      interval = setInterval(onTick, stepMilliseconds)
     }
   }
-  
-  main();
-}());
+
+  function main () {
+    loadFromLocalStorage()
+
+    if (!(player && player.steps)) {
+      resetPlayer()
+    }
+
+    createGraph()
+
+    setupButtons()
+
+    updateUX()
+
+    getHighScores()
+
+    if (!player.won) {
+      startTicks()
+    }
+  }
+
+  main()
+}())
--- server.js
+++ server.js
@@ -1,83 +1,84 @@
-var express = require('express');
-var app = express();
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
-var { getScores, saveScore, resetScores } = require('./scores');
-
-var store = new FileStore({
+const path = require('path')
+const express = require('express')
+const app = express()
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const { getScores, saveScore, resetScores } = require('./scores')
+const store = new FileStore({
   ttl: 5,
   retries: 0,
   path: './sessions'
-});
+})
 
 app.use(session({
-    store: store,
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        // path: "/scores/",
-        // httpOnly: true,
-        // secure: true,
-        sameSite: true
-    },
-    name: 'home-again',
-    maxAge: 10 * 1000,
-    rolling: false,
-  })
-);
+  store: store,
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    // path: "/scores/",
+    // httpOnly: true,
+    // secure: true,
+    sameSite: true
+  },
+  name: 'home-again',
+  maxAge: 10 * 1000,
+  rolling: false
+})
+)
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-app.use(express.json());
+app.use(express.static('public'))
+app.use(express.json())
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response, next) {
-  response.sendFile(__dirname + '/pages/index.html');
-});
+app.get('/', function (request, response, next) {
+  response.sendFile(path.join(__dirname, '/pages/index.html'))
+})
 
 app.get('/scores/', function (req, res) {
-  // console.log("GET /scores/", req.sessionID);
-  if (!req.session.key){
-    req.session.key = new Buffer(Date.now().toString()).toString('base64');
-    // console.log("Created session", req.sessionID);
+  // console.log(`GET /scores/ ${req.sessionID}`);
+  if (!req.session.key) {
+    req.session.key = Buffer.from(Date.now().toString()).toString('base64')
+    // console.log(`Created session ${req.sessionID}`);
   }
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(getScores()));
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify(getScores()))
 })
 
 app.post('/scores/', function (req, res) {
-  let body = req.body,
-    scores;
-//   console.log("POST /scores/", req.sessionID);
-//   console.log(`Got ${JSON.stringify(body)}`);
-  
-  if (!Number.isInteger(body.steps) || body.steps % 2 != 0 || typeof body.name !== "string") {
-    res.status(400);
-    res.send();
-    return;
+  let body = req.body
+
+  let scores
+  //   console.log(`POST /scores/ ${req.sessionID}`);
+  //   console.log(`Got ${JSON.stringify(body)}`);
+
+  if (!Number.isInteger(body.steps) || body.steps % 2 !== 0 || typeof body.name !== 'string') {
+    res.status(400)
+    res.send()
+    return
   }
-  
-  store.get(req.sessionID, function(err, sess) {
-    if (sess && sess.key){
-      scores = saveScore(req.body.steps, req.body.name.slice(0, 20));
-      // console.log("Consuming session", req.sessionID);
-      store.destroy(req.sessionID);
+
+  store.get(req.sessionID, function (err, sess) {
+    if (!err && sess && sess.key) {
+      scores = saveScore(req.body.steps, req.body.name.slice(0, 20))
+      // console.log(`Consuming session ${req.sessionID}`);
+      store.destroy(req.sessionID)
     }
-    
-    req.session.destroy();
-    res.clearCookie('home-again');
-    
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(scores));
-  });
+
+    req.session.destroy()
+    res.clearCookie('home-again')
+
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify(scores))
+  })
 })
 
-if (!getScores().length){
-  resetScores();
+if (!getScores().length) {
+  resetScores()
 }
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+  console.log('Your app is listening on port ' + listener.address().port)
+})
